192063223a6d6aaccf5ec819f41d84b671d2172a
Affan Muddassir <affan.muddassir10@gmail.com>
2020-04-25 22:33:35 +0500

    improved randomize (#5)

--- funcs.py
+++ funcs.py
@@ -2,8 +2,11 @@ import random
 
 main_list = [12, 10, 93, 77, 42, 74, 8, 5, 3]
 
-# Function shuffles a list by creating a copy
 def randomize(list):
-    return random.shuffle(list)
+    rand_list = random.sample(list, len(list))
+    diff_func = lambda a, b: a - b
+    print(list)
+    print(rand_list)
+    return map(diff_func, list, rand_list)
 
-print(randomize(main_list))
+print(list(randomize(main_list)))
