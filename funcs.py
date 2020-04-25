6afd85203ffe5535ef17ccdc709ce7ff337f012a
Affan Muddassir <affan.muddassir10@gmail.com>
2020-04-25 22:22:23 +0500

    Using shuffle to shuffle the list in place

--- funcs.py
+++ funcs.py
@@ -4,6 +4,6 @@ main_list = [12, 10, 93, 77, 42, 74, 8, 5, 3]
 
 # Function shuffles a list by creating a copy
 def randomize(list):
-    return random.sample(list, len(list))
+    return random.shuffle(list)
 
 print(randomize(main_list))
