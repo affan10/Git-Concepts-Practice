e9d198c33751d311d10ef03b0a72f826f33183bf
Affan <affan.muddassir10@gmail.com>
2020-04-25 19:37:20 +0500

    added method level comments

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 main_list = [10, 12, 13, 20, 15, 27, 18, 29, 42]
 
+# Simple squaring of all elements of main_list
 def list_comprehension_mult(list):
     return [x**2 for x in list]
 
 print(list_comprehension_mult(main_list))
 
+# Only square those elements that are even
 def list_comprehension_mult_even(list):
     return [x**2 for x in list if x % 2 == 0]
 
