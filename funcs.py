3f411993bfbe9847dd64c82430b1522e8336b2f4
Affan Muddassir <affan.muddassir10@gmail.com>
2020-04-25 22:53:15 +0500

    Updated randomize

--- funcs.py
+++ funcs.py
@@ -2,11 +2,13 @@ import random
 
 main_list = [12, 10, 93, 77, 42, 74, 8, 5, 3]
 
+# Method level comment for randomize
 def randomize(list):
     rand_list = random.sample(list, len(list))
-    diff_func = lambda a, b: a - b
+    diff_func = lambda a, b: a * b
     print(list)
     print(rand_list)
-    return map(diff_func, list, rand_list)
+    diff_list = list(map(diff_func, list, rand_list))
+    return diff_list
 
-print(list(randomize(main_list)))
+print(randomize(main_list))
