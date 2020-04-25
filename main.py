24c7dc734363cb6d7bf1cc195116cb3e7854ffa4
Affan <affan.muddassir10@gmail.com>
2020-04-25 19:31:20 +0500

    added function for even numbers squaring

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ def list_comprehension_mult(list):
     return [x**2 for x in list]
 
 print(list_comprehension_mult(main_list))
+
+def list_comprehension_mult_even(list):
+    return [x**2 for x in list if x % 2 == 0]
+
+print(list_comprehension_mult_even(main_list))
