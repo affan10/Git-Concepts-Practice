13648e66136b2d2455b52f2794d6544cf77c835d
Affan Muddassir <affan.muddassir10@gmail.com>
2020-04-25 19:42:32 +0500

    Corrected function names - 2

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ main_list = [10, 12, 13, 20, 15, 27, 18, 29, 42]
 def list_comprehension_squared(list):
     return [x**2 for x in list]
 
-print(list_comprehension_mult(main_list))
+print(list_comprehension_squared(main_list))
 
 # Only square those elements that are even
 def list_comprehension_squared_even(list):
     return [x**2 for x in list if x % 2 == 0]
 
-print(list_comprehension_mult_even(main_list))
+print(list_comprehension_squared_even(main_list))
