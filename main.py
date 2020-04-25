f364824c20d3a134de2a78b0183484cffcf48a26
Affan Muddassir <affan.muddassir10@gmail.com>
2020-04-25 20:36:03 +0500

    Changed even function to odd

--- main.py
+++ main.py
@@ -2,12 +2,17 @@ main_list = [10, 12, 13, 20, 15, 27, 18, 29, 42]
 
 # Simple squaring of all elements of main_list
 def list_comprehension_squared(list):
-    return [x**2 for x in list]
+        return [x**3 for x in list]
 
 print(list_comprehension_squared(main_list))
 
 # Return even numbers only
 def list_comprehension_even_numbers(list):
-    return [x for x in list if x % 2 == 0]
+    # For rebasing practice
+    # Check if list is not empty
+    if list:
+        # Floor divide by 2 all odd numbers
+        return [x//2 for x in list if x % 2 != 0]
+    return "List does not exists!"
 
 print(list_comprehension_even_numbers(main_list))
